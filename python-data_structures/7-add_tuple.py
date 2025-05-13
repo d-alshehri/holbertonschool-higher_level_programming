53c470a2b0ee595e1f759c2d909c6e60535191d7
d-alshehri <10674@holbertonstudents.com>
2025-05-13 04:19:34 +0000

    Fixed 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,7 +8,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(a) < 2:
         a.extend([0]*(2-len(a)))
     if len(b) < 2:
-        b.extend([0]*(2-len(b))
+        b.extend([0]*(2-len(b)))
 
     sumA = a[0] + b[0]
     sumB = a[1] + b[1]
