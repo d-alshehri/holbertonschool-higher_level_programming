9decf6b7b40825126b5d2619b2fe3d544a350763
d-alshehri <10674@holbertonstudents.com>
2025-05-13 04:12:21 +0000

    Solved 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = list(tuple_a)
+    b = list(tuple_b)
+
+    if len(a) < 2:
+        a.append(0)
+    if len(b) < 2:
+        b.append(0)
+
+    sumA = a[0] + b[0]
+    sumB = a[1] + b[1]
+
+    return (sumA, sumB)
