370349850c74337d54d34dfebf44ba748ed01d3a
d-alshehri <10674@holbertonstudents.com>
2025-05-13 03:56:08 +0000

    Tried different approach for 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()    
+        print(" ".join("{:d}".format(num) for num in row))
+
