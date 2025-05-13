0948a51b5a3921244033d8faf75723b054baaa5a
d-alshehri <10674@holbertonstudents.com>
2025-05-13 15:01:04 +0000

    Fixed 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+            new_list[i] = replace
+    return new_list
