91d71f031645318249af32436988ca3081b3c2e1
d-alshehri <10674@holbertonstudents.com>
2025-05-13 21:48:50 +0000

    Fixed 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,6 @@ def multiply_by_2(a_dictionary):
     new_dict = dict(a_dictionary)
 
     for key in new_dict:
-        new_dict[key] = my_dict[key] * 2
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
     
