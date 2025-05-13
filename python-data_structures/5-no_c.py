7bd8be84d284ab08126db3cfccbda54b53a59127
d-alshehri <10674@holbertonstudents.com>
2025-05-13 03:35:33 +0000

    Fixed 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     result = ""
     for char in my_string:
-        if char != 'c' or char != 'C':
-            result+=char
+        if char != 'c' and char != 'C':
+            result += char
     return result
