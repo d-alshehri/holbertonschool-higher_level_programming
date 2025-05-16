8619bcd835ebc14a48399ebd97fa0c143969c62e
d-alshehri <10674@holbertonstudents.com>
2025-05-16 19:15:29 +0000

    Solved 4. Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a function that prints text with indentation after punctuation."""
+
+
+def text_indentation(text):
+    """Print text with two newlines after each '.', '?', and ':' punctuation marks.
+
+    Args:
+        text (str): The text to print.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
