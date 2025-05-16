f4d1467bae5ecd4948f3407b5dc78023a495342d
d-alshehri <10674@holbertonstudents.com>
2025-05-16 18:58:51 +0000

    Fixed 0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,13 +7,15 @@ It handles float overflow situations by checking if the number exceeds
 the floating-point limit (infinity), and also checks for NaN (Not a Number).
 """
 def add_integer(a, b=98):
-    """ add two integers or floats """
-    if type(a) is not int and type(a) is not float:
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
