7b3efd0f9984af3708e57fda54957e261d186b82
d-alshehri <10674@holbertonstudents.com>
2025-05-27 14:00:44 +0000

    [4-inherits_from]Added function documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,6 +7,10 @@ otherwise False.
 
 
 def inherits_from(obj, a_class):
+    """
+    Returns True if the object's class is a subclass of the specified class,
+    but not the specified class itself.
+    """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
