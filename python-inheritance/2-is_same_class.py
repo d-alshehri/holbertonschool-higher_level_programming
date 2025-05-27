b017679f72adb9ff275a44f2fe68411878006235
d-alshehri <10674@holbertonstudents.com>
2025-05-27 13:37:07 +0000

    [2-is_same_class]Fixed to validate PEP8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A function that returns True if the object is exactly an instance of the specified class .
+A function that returns True if the object is an instance of the specified class.
 """
 
 
@@ -9,4 +9,4 @@ def is_same_class(obj, a_class):
     Returns True if obj is exactly an instance of a_class,
     otherwise False.
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
