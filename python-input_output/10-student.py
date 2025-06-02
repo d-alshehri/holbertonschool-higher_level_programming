8d25284d78f18a1fcf9689535935cf5fa85ae922
d-alshehri <10674@holbertonstudents.com>
2025-06-02 13:05:35 +0000

    PEP8 validation

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -35,6 +35,8 @@ class Student:
         Returns:
             dict: A dictionary of selected or all attributes.
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        if isinstance(attrs, list) and
+        all(isinstance(attr, str) for attr in attrs):
+            return
+            {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
