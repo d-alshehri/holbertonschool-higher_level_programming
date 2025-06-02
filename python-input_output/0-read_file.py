66978be51a1e9cfe0dda721c63920fd4b298d9a2
d-alshehri <10674@holbertonstudents.com>
2025-06-02 11:47:48 +0000

    PEP8 validation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 Defines a text file-reading function.
 """
 
+
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, "r", encoding="UTF-8") as f:
