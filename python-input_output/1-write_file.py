6e3a775d23955b51efc0347451300cf0437bf69b
d-alshehri <10674@holbertonstudents.com>
2025-06-02 12:11:18 +0000

    Added documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Defines a text file line-counting function.
+Defines a function to count the number of lines in a text file.
 """
 
-
 def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
+    """
+    Counts the number of lines in a text file.
+    """
     lines = 0
     with open(filename) as f:
         for line in f:
