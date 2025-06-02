ccc6d3f30565d9f07fdf73e46a7c012c37a1ab12
d-alshehri <10674@holbertonstudents.com>
2025-06-02 12:45:28 +0000

    PEP8 validation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,7 +6,10 @@ Module that defines a function to convert an object to a JSON string.
 
 import json
 
+
 def to_json_string(my_obj):
+
+    
     """
     Returns the JSON representation of an object (string).
 
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,7 +8,10 @@ Module that defines a function to convert a JSON string to a Python object.
 
 import json
 
+
 def from_json_string(my_str):
+
+    
     """
     Returns a Python object represented by a JSON string.
 
