28fe5ff3520613a24292c53dea4ae26b8f2b02ae
d-alshehri <10674@holbertonstudents.com>
2025-06-02 12:43:38 +0000

    [4]Add function to deserialize a JSON string to a Python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to convert a JSON string to a Python object.
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Returns a Python object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to deserialize.
+
+    Returns:
+        object: The corresponding Python object.
+    """
+    return json.loads(my_str)
