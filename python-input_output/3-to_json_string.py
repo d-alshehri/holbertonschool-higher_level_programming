10df83c93c5a3c00fb6ccabf9c6bfd91595fdf8d
d-alshehri <10674@holbertonstudents.com>
2025-06-02 12:42:10 +0000

    [3]Add function to convert Python object to JSON string using json.dumps

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to convert an object to a JSON string.
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The Python object to serialize.
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
