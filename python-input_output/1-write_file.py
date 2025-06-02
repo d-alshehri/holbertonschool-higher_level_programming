48bb2745c282fbeb5a869c12bf2155f838c24e3c
d-alshehri <10674@holbertonstudents.com>
2025-06-02 12:13:24 +0000

    PEP8 validation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,9 +3,11 @@
 Module that defines a function to write a string to a text file.
 """
 
+
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Writes a string to a text file (UTF8) 
+    and returns the number of characters written.
 
     Args:
         filename (str): The name of the file to write to.
