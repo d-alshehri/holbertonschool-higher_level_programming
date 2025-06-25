6f3098dbbcf6256b59bdc149cbf631bc24070693
d-alshehri <10674@holbertonstudents.com>
2025-06-25 06:23:06 +0000

    Fixed task 0 code

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server 
-
--- Create users if they don’t exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Show privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
