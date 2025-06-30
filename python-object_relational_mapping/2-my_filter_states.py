fb9532e2f0db445e7faf91f6190c5b941dc2ecfa
d-alshehri <10674@holbertonstudents.com>
2025-06-30 06:11:09 +0000

    Fixed task 2 code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""Script that lists all values in the states table where name matches the argument."""
-
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Extract arguments
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-
-    # WARNING: vulnerable to SQL injection (we’ll fix this in task 3)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
-
-    results = cur.fetchall()
-    for row in results:
-        print(row)
-
-    # Cleanup
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
